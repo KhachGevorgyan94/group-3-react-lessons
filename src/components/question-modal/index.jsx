f39b649272fe4ac75e729c401169272a7dde836b
khachgevorgyan94 <khachgevorgyan94@gmail.com>
2023-04-15 18:59:20 +0400

    update

--- src/components/question-modal/index.jsx
+++
@@ -1,9 +0,0 @@
-const QuestionModal = ({closeModal})=>{
-  return <div>
-    <h1> Are you sure to log out from project??</h1>
-
-    <button onClick={()=>closeModal(false)}>Cancel</button>
-    <button>Accept</button>
-  </div>
-}
-export default QuestionModal
